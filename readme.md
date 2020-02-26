046e8b3c05489fd9e41bf70e370f719611c8b414
Luis Angel Cairampoma Arbildo <luis.cairampoma@autodesk.com>
2020-02-26 17:38:59 -0500

    init project

---
+++ readme.md
@@ -0,0 +1,31 @@
+> ClojureScript optimized for modern React development.
+
+## Quick start
+
+You will need [Node.js](https://nodejs.org/en/) and [Clojure CLI tools](https://clojure.org/guides/getting_started) installed on your machine.
+
+```
+npm i
+
+npm start
+```
+
+Navigate to http://localhost:8888
+
+
+![](https://github.com/tastejs/todomvc-app-css/raw/master/screenshot.png)
+
+## Resources + Support
+
+- [Helix GitHub Repo](https://github.com/Lokeh/helix)
+- [Slack](https://clojurians.net) channel: `#hx`
+
+
+<!-- ## Implementation -->
+
+<!-- How was the app created? Anything worth sharing about the process of creating the app? Any spec violations? -->
+
+
+## Credit
+
+Created by Luis C.
---
+++ src/map_science/core.cljs
@@ -0,0 +1,48 @@
+(ns map-science.core
+  (:require	[clojure.string :as string]
+            [helix.core :as hx :refer [$ <> defnc]]
+            [helix.dom :as d]
+            [helix.hooks :as hooks]
+            ["pigeon-maps" :as Map]
+            ["pigeon-overlay" :default Overlay]
+            ["react-dom" :as rdom]
+            ["react-router-dom" :as rr]))
+
+
+(def scientists
+  [{
+    :id 18079
+    :name "Leonardo Da Vinci"
+    :img "undefined"
+    :bplace_geonameid {:lat 43.783 :lon 11.25}}
+   {
+    :id 18021
+    :name "Luis C."
+    :img "undefined"
+    :bplace_geonameid {:lat 27.783 :lon 1.25}}])
+
+(defnc AppMap
+  [{:keys [scientists]}]
+  (js/console.log scientists)
+  ($ Map {:width 700 :height 700 :center #js [50 4] :zoom 2.3}
+     (map-indexed #(let [scientist %2 index %]
+                     ($ Overlay
+                        {:anchor #js [
+                                      (:lat  (:bplace_geonameid scientist))
+                                      (:lon (:bplace_geonameid scientist))]
+                                 :key index}
+                        (d/img {:src (str "https://pantheon.world/images/profile/people/" (:id scientist) ".jpg")
+                                :style {:width 20}})))
+          scientists)))
+
+(defnc App []
+  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
+    (d/div
+     (d/h1 "Science Timeline!!!")
+     ;; create elements out of components
+     ($ AppMap {:scientists scientists}))))
+
+
+(defn ^:export start
+  []
+  (rdom/render ($ App) (js/document.getElementById "app")))
